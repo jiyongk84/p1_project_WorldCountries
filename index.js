27a1941d4fb671f7ae38c8697afc4a8554d6ede5
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-05-14 01:30:36 -0400

    index.js receives all data from api

--- index.js
+++ index.js
@@ -7,23 +7,49 @@ function worldCountries() {
     })
 }    
 
-function listOneCountry(country) {
-    let card = document.createElement('li')
-    card.className = 'card'
-    card.innerHTML = `
-        <div class="country">
-            <h3>${country.name.common}</h3>
-        <p> Capital City: ${country.capital} </p>
-        <img src="${country.flags.png}">
-        </div> `
-     
-    document.querySelector('#country-list').appendChild(card)
-}
+const searchBox = document.querySelector('#inputBox')
+const submitButton = document.querySelector('.submit')
+//const resultDiv = document.querySelector('#country-cards');
 
 function getCountries() {
+        const inputValue = searchBox.value.trim();
+      
+        if (inputValue === '') {
+          resultDiv.textContent = 'Please enter a search term.';
+          return;
+        }
     fetch('https://restcountries.com/v3.1/all')
     .then((resp) => resp.json())
-    .then((countryData) => countryData.forEach(country => listOneCountry(country)))
-}
+    .then(countryData => {
+            const matchingData = countryData.map(item => item.name.common.toLowerCase() === inputValue.toLowerCase() ? item : null).filter(Boolean);
+            if (matchingData.length > 0) {
+              console.log('Match found for ' + inputValue);
+              listOneCountry(matchingData.shift());
+            } else {
+              console.log('No match found for ' + inputValue);
+              displayNoResult();
+            }
+          })
+          //.catch(error => console.error('Error fetching data: ' + error));
+        
+      }//);
+      function listOneCountry(country) {
+        let card = document.createElement('li')
+        card.className = 'card'
+        card.innerHTML = `
+            <div class="country">
+                <h3>${country.name.common}</h3>
+            <p> Capital City: ${country.capital} </p>
+            <p> Official Language(s): ${Object.values(country.languages)}</p>
+            <p> Continent: ${country.region} </p>
+            <img src="${country.flags.png}">
+            </div> `
+         
+        document.querySelector('#country-list').appendChild(card)
+    }
 
-worldCountries()
+    function displayNoResult() {
+        alert('No match found. Try again')
+      }
+
+    worldCountries()
